46849de8204ea63ed3f58635d6b6793543160985
raoul2000 <manu34@free.fr>
2020-02-22 17:11:08 +0100

    initial import

---
+++ src/models/Book.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%book}}".
+ *
+ * @property int $id
+ * @property string $title
+ */
+class Book extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%book}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['title'], 'string', 'max' => 128],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'title' => Yii::t('app', 'Title'),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return BookQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new BookQuery(get_called_class());
+    }
+}
