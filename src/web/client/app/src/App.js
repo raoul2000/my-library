13181df33482218f60ff20cbd574a48bcacde586
raoul2000 <manu34@free.fr>
2020-02-23 13:56:58 +0100

    add material-ui dependency

--- src/web/client/app/src/App.js
+++ src/web/client/app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-
+import Button from '@material-ui/core/Button';
 import BookList from './components/BookList';
-
 
 function App() {
   return (
     <div className="App">
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
       <BookList />
     </div>
   );
